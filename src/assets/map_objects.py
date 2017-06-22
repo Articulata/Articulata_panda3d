c2071aefe1bb7921c6d9642c7bb7ece78026d707
Adam Vandervorst <adam.vandervorst@gmail.com>
2017-06-22 23:41:09 +0200

    Added assets

---
+++ src/assets/map_objects.py
@@ -0,0 +1,11 @@
+from direct.showbase.DirectObject import DirectObject
+from panda3d.core import *
+
+
+class Castle(DirectObject):
+    def __init__(self, pos, hpr, sc):
+        self.model = loader.loadModel("models/castle")
+        self.model.setPos(pos.getX(), pos.getY(), pos.getZ())
+        self.model.setHpr(hpr.getX(), hpr.getY(), hpr.getZ())
+        self.model.setScale(sc)
+        self.model.reparentTo(render)
