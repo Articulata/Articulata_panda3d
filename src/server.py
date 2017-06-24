ecabea3681b26e45e073bb6d9749b87e2cd47626
Adam Vandervorst <adam.vandervorst@gmail.com>
2017-06-24 21:18:04 +0200

    Made server less complicated and a little more complex

--- src/server.py
+++ src/server.py
@@ -45,121 +45,45 @@ class Server(QueuedConnectionManager):
         self.backlog = b
         self.socket = self.cManager.openTCPServerRendezvous(self.port, self.backlog)
         self.cListener.addConnection(self.socket)
-        self.num_count = 0
+        self.player_list = []
+        self.iterator = None
+        self.num_count = self.active_players = self.Δt_update = self.Δt = 0
 
-    def tskReaderPolling(self, regClass):
+    def reader_polling_task(self, arg):
         if self.cReader.dataAvailable():
             datagram = NetDatagram()
 
             if self.cReader.getData(datagram):
-                regClass.updateData(datagram.getConnection(), datagram, self)
+                self.process_data(datagram.getConnection(), datagram)
 
         return Task.cont
 
-    def tskListenerPolling(self, reg_class):
+    def listener_polling_task(self, arg):
         if self.cListener.newConnectionAvailable():
             rendezvous = PointerToConnection()
             net_address = NetAddress()
             new_connection = PointerToConnection()
             if self.cListener.getNewConnection(rendezvous, net_address, new_connection):
-                reg_class.active_players += 1
+                self.active_players += 1
                 new_connection = new_connection.p()
-                reg_class.player_list.append(player(self.num_count))
-                find_player(reg_class.player_list, "player_id", self.num_count).conn_id = new_connection
-                logging.debug(reg_class.active_players)
-                reg_class.sendInitialInfo(reg_class.active_players, self)
+                self.player_list.append(player(self.num_count))
+                find_player(self.player_list, "player_id", self.num_count).conn_id = new_connection
+                logging.debug(self.active_players)
+                self.initialize_player()
                 self.num_count += 1
                 self.cReader.addConnection(new_connection)  # Begin reading connection
                 logging.info('connection received')
         return Task.cont
 
-
-class PlayerReg(DirectObject):
-    """Registers new players and keeps existing ones up to date"""
-    def __init__(self):
-        self.player_list = []
-        self.num_count = self.active_players = self.Δt_update = self.Δt = 0
-
-    def updatePlayers(self, server_class, data, msg_type):  # send
-        if msg_type == "positions":
-            self.Δt = globalClock.getDt()
-            self.Δt_update += self.Δt
-            if self.Δt_update > 0.05:
-                if self.active_players > 1:
-                    datagram = PyDatagram()
-                    datagram.addString("update")
-                    datagram.addInt8(self.active_players)
-
-                    for p in self.player_list:
-                        datagram.addString(p.username)
-                        datagram.addFloat64(p.pos_and_or['x'])
-                        datagram.addFloat64(p.pos_and_or['y'])
-                        datagram.addFloat64(p.pos_and_or['z'])
-                        datagram.addFloat64(p.pos_and_or['h'])
-                        datagram.addFloat64(p.pos_and_or['p'])
-                        datagram.addFloat64(p.pos_and_or['r'])
-
-                    for p in self.player_list:
-                        server_class.cWriter.send(datagram, p.conn_id)
-                self.Δt_update = 0
-            return Task.cont
-        elif msg_type == "chat":
-            datagram = PyDatagram()
-            text = data.getString()
-
-            datagram.addString("chat")
-            datagram.addString(text)
-
-            logging.info(f"{text} {server_class}")
-            for p in self.player_list:
-                server_class.cWriter.send(datagram, p.conn_id)
-
-    def updateData(self, connection, datagram, server_class):
-        iterator = PyDatagramIterator(datagram)
-        msg_type = iterator.getString()
-        if msg_type == "position":
-            pos_and_or = find_player(self.player_list, "conn_id", connection).pos_and_or
-            pos_and_or['x'] = iterator.getFloat64()
-            pos_and_or['y'] = iterator.getFloat64()
-            pos_and_or['z'] = iterator.getFloat64()
-            pos_and_or['h'] = iterator.getFloat64()
-            pos_and_or['p'] = iterator.getFloat64()
-            pos_and_or['r'] = iterator.getFloat64()
-        elif msg_type == "chat":
-            self.updatePlayers(server_class, iterator, "chat")
-        elif msg_type == "introduce":
-            username = iterator.getString()
-            logging.info(f"User {username} introduced himself")
-            cls = find_player(self.player_list, "conn_id", connection)
-            cls.username = username
-        elif msg_type == "quit":
-            logging.debug("Player has quit")
-            self.active_players -= 1
-
-            player_num = iterator.getInt8()
-            player_cls = find_player(self.player_list, "player_id", player_num)
-            player_id = self.player_list.index(player_cls)
-            player_name = player_cls.username
-            del self.player_list[player_id]
-
-            datagram = PyDatagram()
-            datagram.addString("remove")
-            datagram.addString(player_name)
-
-            for p in self.player_list:
-                server_class.cWriter.send(datagram, p.conn_id)
-
-            logging.info(f"Player {player_name} has left the game")
-
-    def sendInitialInfo(self, num_players, server):
+    def initialize_player(self):
         conn = self.player_list[-1].conn_id
-        
+
         datagram = PyDatagram()
         datagram.addString("init")
         newest = self.player_list[-1].player_id
         datagram.addUint8(newest)
-        logging.debug(f"{num_players} players and {newest} is the newest player")
-        datagram.addUint8(num_players)
+        logging.debug(f"{self.active_players} players and {newest} is the newest player")
+        datagram.addUint8(self.active_players)
 
         if len(self.player_list) > 1:
             for k in self.player_list:
@@ -168,7 +92,85 @@ class PlayerReg(DirectObject):
                 datagram.addFloat64(k.pos_and_or['y'])
                 datagram.addFloat64(k.pos_and_or['z'])
 
-        server.cWriter.send(datagram, conn)
+        self.cWriter.send(datagram, conn)
+
+    def update_positions(self, data):  # send
+        self.Δt = globalClock.getDt()
+        self.Δt_update += self.Δt
+        if self.Δt_update > 0.05:
+            if self.active_players > 1:
+                datagram = PyDatagram()
+                datagram.addString("update")
+                datagram.addInt8(self.active_players)
+
+                for p in self.player_list:
+                    datagram.addString(p.username)
+                    datagram.addFloat64(p.pos_and_or['x'])
+                    datagram.addFloat64(p.pos_and_or['y'])
+                    datagram.addFloat64(p.pos_and_or['z'])
+                    datagram.addFloat64(p.pos_and_or['h'])
+                    datagram.addFloat64(p.pos_and_or['p'])
+                    datagram.addFloat64(p.pos_and_or['r'])
+
+                for p in self.player_list:
+                    self.cWriter.send(datagram, p.conn_id)
+            self.Δt_update = 0
+        return Task.cont
+
+    def update_chat(self, msg):
+        datagram = PyDatagram()
+
+        datagram.addString("chat")
+        datagram.addString(msg)
+
+        logging.info(f"{msg}")  # TODO make this useful
+        for p in self.player_list:
+            self.cWriter.send(datagram, p.conn_id)
+
+    def process_data(self, connection, datagram):
+        self.iterator = PyDatagramIterator(datagram)
+        msg_type = self.iterator.getString()
+
+        if msg_type == "position":
+            self.process_position(connection)
+        elif msg_type == "chat":
+            self.update_chat(self.iterator.getString())
+        elif msg_type == "introduce":
+            username = self.iterator.getString()
+            logging.info(f"User {username} introduced himself")
+            cls = find_player(self.player_list, "conn_id", connection)
+            cls.username = username
+        elif msg_type == "quit":
+            self.process_quit()
+
+    def process_position(self, connection):
+        it = self.iterator
+        pos_and_or = find_player(self.player_list, "conn_id", connection).pos_and_or
+        pos_and_or['x'] = it.getFloat64()
+        pos_and_or['y'] = it.getFloat64()
+        pos_and_or['z'] = it.getFloat64()
+        pos_and_or['h'] = it.getFloat64()
+        pos_and_or['p'] = it.getFloat64()
+        pos_and_or['r'] = it.getFloat64()
+
+    def process_quit(self):
+        logging.debug("Player has quit")
+        self.active_players -= 1
+
+        player_num = self.iterator.getInt8()
+        player_cls = find_player(self.player_list, "player_id", player_num)
+        player_id = self.player_list.index(player_cls)
+        player_name = player_cls.username
+        del self.player_list[player_id]
+
+        datagram = PyDatagram()
+        datagram.addString("remove")
+        datagram.addString(player_name)
+
+        for p in self.player_list:
+            self.cWriter.send(datagram, p.conn_id)
+
+        logging.info(f"Player {player_name} has left the game")
 
 
 class player(DirectObject):
@@ -180,14 +182,12 @@ class player(DirectObject):
         self.moving = False  # TODO
 
 
-worldServer = Server(9415, 1000)
+server = Server(9415, 1000)
 
-Active = PlayerReg()
-
-taskMgr.add(worldServer.tskListenerPolling, "Poll the connection listener", extraArgs=[Active])
-taskMgr.add(worldServer.tskReaderPolling, "Poll the connection reader", extraArgs=[Active])
+taskMgr.add(server.listener_polling_task, "Poll the connection listener")
+taskMgr.add(server.reader_polling_task, "Poll the connection reader")
 if args.mp:
-    taskMgr.add(Active.updatePlayers, "Update Every Player", extraArgs=[worldServer, None, "positions"])
+    taskMgr.add(server.update_positions, "Update Every Player", extraArgs=[None])
 
 logging.info("started")
 base.run()
